3708e250bd8f549e7f3b5adf806ccb50ce5766da
Olga Luisa <olgaluiisa@hotmail.com>
2022-10-22 04:06:48 -0300

    feat: installing styled-components, react-icons and creating light and dark themes

---
+++ src/styles/theme.jsx
@@ -0,0 +1,19 @@
+export const lightTheme = {
+  body: "#CECACA",
+  calculatorBackground: "#F6F6F6",
+  screenBackground:"#EFEFF5",
+  buttonBackground:"#EFEFF5",
+  buttonBackgroundDark:"#8B8BFF",
+  fontColorIcons:"#8B8BFF",
+  fontColorNumbers:"#121214"
+}
+
+export const darkTheme = {
+  body: "#CECACA",
+  calculatorBackground: "#121214",
+  screenBackground:"#121214",
+  buttonBackground:"#EFEFF5",
+  buttonBackgroundDark:"#8B8BFF",
+  fontColorIcons:"#EFEFF5",
+  fontColorNumbers:"#8B8BFF"
+}
