97f2b0cc976847aad3d6adb0062a62a84dd161c7
Olga Luisa <olgaluiisa@hotmail.com>
2022-10-23 00:16:43 -0300

    hotfix: Component button styling tweaks

--- src/Components/Button/style.jsx
+++ src/Components/Button/style.jsx
@@ -1,13 +1,37 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 
 export const Container = styled.button`
 width: 48px;
 height: 48px;
 border: none;
 border-radius: 42%;
-background-color: ${props => props.theme.buttonBackground};
-color: ${props => props.theme.fontColorNumbers};
 box-shadow: 0 4px 4px 0 rgba(0,0,0,0.25) ;
 cursor: pointer;
 margin: 10px;
+
+${props => props.buttonIconsBackgroundLigth && css `
+   background: #EFEFF5;
+   color:#8B8BFF ;
+`}
+${props => props.buttonNumberBackgroundLigth && css `
+   background: #EFEFF5;
+   color:#121214;
+`}
+${props => props.buttonIconsBackgroundDark && css `
+   background:#8B8BFF;
+   color:#EFEFF5 ;
+`}
+${props => props.buttonNumberBackgroundDark && css `
+   background: #EFEFF5;
+   color:#8B8BFF;
+`}
+
+${props => props.buttonEqualBackgroundLigth && css `
+    background:#8B8BFF;
+    color:#EFEFF5 ;
+`}
+${props => props.buttonEqualBackgroundDark && css `
+    background: #EFEFF5;
+    color:#8B8BFF;
+`}
 `
