b69409b20818c5e0f48a85ca3d11173834c850b9
Olga Luisa <olgaluiisa@hotmail.com>
2022-10-25 00:31:22 -0300

    hotfix: correction of some functions

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -61,7 +61,7 @@ export const Calculator = () => {
 
       {theme === "dark" ?  <Button roxo onClick={()=> handleIconsClick("+")}><RiAddLine size={"50px"}/></Button> :  <Button branco onClick={()=> handleIconsClick("+")}><RiAddLine size={"50px"}/></Button>}
 
-      {theme === "dark" ?  <Button roxo onClick={deleteNumber} ><RiDeleteBack2Line size={"40px"}/></Button> : <Button branco value={"del"}><RiDeleteBack2Line size={"40px"}/></Button>}
+      {theme === "dark" ?  <Button roxo onClick={deleteNumber} ><RiDeleteBack2Line size={"40px"}/></Button> : <Button branco onClick={deleteNumber}><RiDeleteBack2Line size={"40px"}/></Button>}
 
       {theme === "dark" ?  <Button roxoNumber onClick={handleValue} value={0}>0</Button> : <Button onClick={handleValue} value={0}>0</Button>}
 
