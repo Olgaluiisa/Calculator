977216aa77cf21b6a3c56f23a75a561962ab3db0
Olga Luisa <olgaluiisa@hotmail.com>
2022-10-25 01:02:12 -0300

    hotfix: settings

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -8,7 +8,7 @@ import { useButton } from "../../Provider/Button"
 
 export const Calculator = () => {
   const {theme,themeToggler} = useTheme()
-  const {handleValue,valueButton,clear,resultValue,porcentage,handleIconsClick,calculator,changePositiveOrNegativeValue,deleteNumber} = useButton()
+  const {valueNumber,valueButton,clear,resultValue,porcentage,valueIcons,calculator,changePositiveOrNegativeValue,deleteNumber} = useButton()
   return (
     <Container>
       {theme === "dark" ?<ContainerScreen dark>
@@ -35,37 +35,37 @@ export const Calculator = () => {
 
       {theme === "dark" ? <Button roxo onClick={porcentage}><RiPercentLine size={"40px"}/></Button> : <Button branco onClick={porcentage}><RiPercentLine size={"40px"}/></Button>}
 
-      {theme === "dark" ? <Button roxo onClick={()=> handleIconsClick("/")}><RiDivideLine size={"50px"} /></Button> :  <Button branco onClick={()=> handleIconsClick("/")}><RiDivideLine size={"50px"}/></Button>}
+      {theme === "dark" ? <Button roxo onClick={()=> valueIcons("/")}><RiDivideLine size={"50px"} /></Button> :  <Button branco onClick={()=> valueIcons("/")}><RiDivideLine size={"50px"}/></Button>}
 
-      {theme === "dark" ?  <Button roxoNumber onClick={handleValue} value={7}>7</Button> :  <Button onClick={handleValue} value={7}>7</Button>}
+      {theme === "dark" ?  <Button roxoNumber onClick={valueNumber} value={7}>7</Button> :  <Button onClick={valueNumber} value={7}>7</Button>}
 
-      {theme === "dark" ?  <Button roxoNumber onClick={handleValue} value={8}>8</Button> :  <Button onClick={handleValue} value={8}>8</Button>}
+      {theme === "dark" ?  <Button roxoNumber onClick={valueNumber} value={8}>8</Button> :  <Button onClick={valueNumber} value={8}>8</Button>}
 
-      {theme === "dark" ?  <Button roxoNumber onClick={handleValue} value={9}>9</Button> :  <Button onClick={handleValue} value={9}>9</Button>}
+      {theme === "dark" ?  <Button roxoNumber onClick={valueNumber} value={9}>9</Button> :  <Button onClick={valueNumber} value={9}>9</Button>}
 
-      {theme === "dark" ?  <Button roxo onClick={()=> handleIconsClick("x")}><RiCloseLine size={"50px"}/></Button> :  <Button branco onClick={()=> handleIconsClick("x")}><RiCloseLine size={"50px"}/></Button>}
+      {theme === "dark" ?  <Button roxo onClick={()=> valueIcons("x")}><RiCloseLine size={"50px"}/></Button> :  <Button branco onClick={()=> valueIcons("x")}><RiCloseLine size={"50px"}/></Button>}
 
-      {theme === "dark" ?  <Button roxoNumber onClick={handleValue} value={4}>4</Button> :  <Button onClick={handleValue} value={4}>4</Button>}
+      {theme === "dark" ?  <Button roxoNumber onClick={valueNumber} value={4}>4</Button> :  <Button onClick={valueNumber} value={4}>4</Button>}
 
-      {theme === "dark" ?  <Button roxoNumber onClick={handleValue} value={5}>5</Button> :  <Button onClick={handleValue} value={5}>5</Button>}
+      {theme === "dark" ?  <Button roxoNumber onClick={valueNumber} value={5}>5</Button> :  <Button onClick={valueNumber} value={5}>5</Button>}
 
-      {theme === "dark" ?  <Button roxoNumber onClick={handleValue} value={6}>6</Button> :  <Button onClick={handleValue} value={6}>6</Button>}
+      {theme === "dark" ?  <Button roxoNumber onClick={valueNumber} value={6}>6</Button> :  <Button onClick={valueNumber} value={6}>6</Button>}
 
-      {theme === "dark" ?  <Button roxo onClick={()=> handleIconsClick("-")}><RiSubtractLine size={"50px"}/></Button> :  <Button branco onClick={()=> handleIconsClick("-")}><RiSubtractLine size={"50px"}/></Button>}
+      {theme === "dark" ?  <Button roxo onClick={()=> valueIcons("-")}><RiSubtractLine size={"50px"}/></Button> :  <Button branco onClick={()=> valueIcons("-")}><RiSubtractLine size={"50px"}/></Button>}
 
-      {theme === "dark" ?  <Button roxoNumber onClick={handleValue} value={1}>1</Button> :  <Button onClick={handleValue} value={1}>1</Button>}
+      {theme === "dark" ?  <Button roxoNumber onClick={valueNumber} value={1}>1</Button> :  <Button onClick={valueNumber} value={1}>1</Button>}
 
-      {theme === "dark" ?  <Button roxoNumber onClick={handleValue} value={2}>2</Button> :  <Button onClick={handleValue} value={2}>2</Button>}
+      {theme === "dark" ?  <Button roxoNumber onClick={valueNumber} value={2}>2</Button> :  <Button onClick={valueNumber} value={2}>2</Button>}
 
-      {theme === "dark" ?  <Button roxoNumber onClick={handleValue} value={3}>3</Button> :  <Button onClick={handleValue} value={3}>3</Button>}
+      {theme === "dark" ?  <Button roxoNumber onClick={valueNumber} value={3}>3</Button> :  <Button onClick={valueNumber} value={3}>3</Button>}
 
-      {theme === "dark" ?  <Button roxo onClick={()=> handleIconsClick("+")}><RiAddLine size={"50px"}/></Button> :  <Button branco onClick={()=> handleIconsClick("+")}><RiAddLine size={"50px"}/></Button>}
+      {theme === "dark" ?  <Button roxo onClick={()=> valueIcons("+")}><RiAddLine size={"50px"}/></Button> :  <Button branco onClick={()=> valueIcons("+")}><RiAddLine size={"50px"}/></Button>}
 
       {theme === "dark" ?  <Button roxo onClick={deleteNumber} ><RiDeleteBack2Line size={"40px"}/></Button> : <Button branco onClick={deleteNumber}><RiDeleteBack2Line size={"40px"}/></Button>}
 
-      {theme === "dark" ?  <Button roxoNumber onClick={handleValue} value={0}>0</Button> : <Button onClick={handleValue} value={0}>0</Button>}
+      {theme === "dark" ?  <Button roxoNumber onClick={valueNumber} value={0}>0</Button> : <Button onClick={valueNumber} value={0}>0</Button>}
 
-      {theme === "dark" ?  <Button roxo onClick={()=> handleIconsClick(".")}><TbWifi0 size={"50px"} /></Button> : <Button branco onClick={()=> handleIconsClick(".")}><TbWifi0 size={"50px"}/></Button>}
+      {theme === "dark" ?  <Button roxo onClick={()=> valueIcons(".")}><TbWifi0 size={"50px"} /></Button> : <Button branco onClick={()=> valueIcons(".")}><TbWifi0 size={"50px"}/></Button>}
 
       {theme === "dark" ? <Button roxoNumber onClick={calculator}><TbEqual size={"40px"}/></Button> : <Button roxo onClick={calculator}><TbEqual size={"40px"}/></Button>}
       </ButtonsContainer>
